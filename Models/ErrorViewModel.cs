901efb8e10e9c189e463731d7cf348a7c260ede2
Pentor <pentorp7989@gmail.com>
2026-02-27 17:19:57 +0700

    new fitmatch

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WEBAPP_FitMatch.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
