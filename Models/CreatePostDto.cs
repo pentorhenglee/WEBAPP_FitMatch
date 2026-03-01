15ee92c0ce462cf702ce339c3617f6c5582512e8
Pentor <pentorp7989@gmail.com>
2026-03-01 14:47:17 +0700

    update Post

--- Models/CreatePostDto.cs
+++ Models/CreatePostDto.cs
@@ -8,7 +8,6 @@ namespace WEBAPP_FitMatch.Models
         public string? Title {get;set;}
         public string? Location {get;set;}
         public DateTime DateTime {get;set;}
-        
         public string? Description {get;set;}
         public string? SportType {get;set;}
         public Int32 MaxPeople {get;set;}
