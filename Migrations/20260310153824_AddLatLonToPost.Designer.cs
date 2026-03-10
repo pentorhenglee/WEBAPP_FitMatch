e6edd064a992f84b1429a82229c605599e583ca6
Pentor <pentorp7989@gmail.com>
2026-03-11 00:42:21 +0700

    add API for location map and forecast weather

--- Migrations/20260310153824_AddLatLonToPost.Designer.cs
+++ Migrations/20260310153824_AddLatLonToPost.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WEBAPP_FitMatch.Data;
@@ -11,9 +12,11 @@ using WEBAPP_FitMatch.Data;
 namespace WEBAPP_FitMatch.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260310153824_AddLatLonToPost")]
+    partial class AddLatLonToPost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
